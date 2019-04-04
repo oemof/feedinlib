e9783080607b65ca81a785aaee9d5e575d9d39ce
Bachibouzouk <pierre-francois.duc@netplus.ch>
2019-04-04 15:00:18 +0200

    Define function to bind the formatting and request sending functions

--- feedinlib/era5.py
+++ feedinlib/era5.py
@@ -138,3 +138,49 @@ def _format_cds_request_position(latitude, longitude, grid=None):
     return answer
 
 
+def _get_cds_data_from_datespan_and_position(
+        start_date,
+        end_date,
+        latitude,
+        longitude,
+        grid=None,
+        dataset_name='reanalysis-era5-single-levels',
+        target_file=None,
+        chunks=None,
+        cds_client=None,
+        **cds_params):
+    """Format request for data from the Climate Data Store (CDS)
+
+        see _get_cds_data() for prior requirements and more information
+
+    :param start_date: (str) start date of the range in YYYY-MM-DD format
+    :param end_date: (str) end date of the range in YYYY-MM-DD format
+    :param latitude: (number)
+    :param longitude: (number)
+    :param dataset_name: (str) short name of the dataset of the CDS. To find it, click on a dataset
+    found in https://cds.climate.copernicus.eu/cdsapp#!/search?type=dataset and go under the
+    ’Download data’ tab, then scroll down the page and click on ’Show API request’, the short
+    name is the string on the 6th line after 'c.retrieve('
+    :param target_file: (str) name of the file in which downloading the data locally
+    :param chunks: (dict)
+    :param cds_client: handle to CDS client (if none is provided, then it is created)
+    :param cds_params: (dict) parameter to pass to the CDS request
+    :return: CDS data in an xarray format
+    """
+
+    kwargs = locals()
+    # Get the formatted year, month and day parameter from the datespan
+    request_dates = _format_cds_request_datespan(start_date, end_date)
+    cds_params.update(request_dates)
+    # Get the area corresponding to a position on the globe for a given grid size
+    request_area = _format_cds_request_position(latitude, longitude, grid)
+    cds_params.update(request_area)
+    # Remove the arguments which will not be passed to the _get_cds_data function
+    kwargs.pop('start_date')
+    kwargs.pop('end_date')
+    kwargs.pop('longitude')
+    kwargs.pop('latitude')
+    kwargs.pop('grid')
+
+    return _get_cds_data(**kwargs)
+

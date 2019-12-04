5a40d1d6bf5fdee4a15b3ee46dabedc0ad244376
Francesco Witte <francesco.witte@hs-flensburg.de>
2019-12-04 16:44:22 +0100

    Add missing oedialect dependency

--- setup.py
+++ setup.py
@@ -21,6 +21,7 @@ setup(
     install_requires=[
         "cdsapi >= 0.1.4",
         "numpy >= 1.7.0",
+        "oedialect",
         "open_FRED-cli",
         "pandas >= 0.13.1",
         "pvlib >= 0.6.0",

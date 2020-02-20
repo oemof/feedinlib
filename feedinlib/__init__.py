a0094c7d277b289cb6f5222d184d081b0f7eea5f
birgits <birgit.schachler@rl-institut.de>
2020-02-20 10:14:37 +0100

    Change license

--- feedinlib/__init__.py
+++ feedinlib/__init__.py
@@ -1,5 +1,5 @@
 __copyright__ = "Copyright oemof developer group"
-__license__ = "GPLv3"
+__license__ = "MIT"
 __version__ = '0.1.0rc3'
 
 from feedinlib.powerplants import Photovoltaic, WindPowerPlant

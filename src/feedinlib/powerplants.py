381b4028f9fce935f8d23af7eb3f2ee3124328fe
uvchik <uvchik.git@posteo.eu>
2021-06-18 10:36:04 +0200

    Remove correct fix, because it is the wrong branch for such changes

--- src/feedinlib/powerplants.py
+++ src/feedinlib/powerplants.py
@@ -371,7 +371,7 @@ class WindPowerPlant(Base):
         if scaling:
             feedin_scaling = {
                 "nominal_power": lambda feedin: feedin
-                / float(self.nominal_power) * kwargs.get("scaling_value", 1)
+                / float(self.nominal_power)
             }
             return feedin_scaling[scaling](feedin)
         return feedin
